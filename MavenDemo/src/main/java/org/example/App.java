878b5a9c46ea6448897be247622f5f5ad6d4c62e
wen <2195322633@qq.com>
2022-11-27 17:12:19 +0800

    ssh commit

--- MavenDemo/src/main/java/org/example/App.java
+++ MavenDemo/src/main/java/org/example/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("git hot fix ssh");
+        System.out.println("git hot fix ssh-git");
     }
 }

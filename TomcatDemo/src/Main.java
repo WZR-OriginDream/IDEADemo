8b2b929a0f4e76f3628a853993bb36403eea57aa
wen <2195322633@qq.com>
2022-11-27 16:28:39 +0800

    first commit

---
+++ TomcatDemo/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author:wzr
+ * @Description:TODO
+ * @Create:${DATE} - ${TIME}
+ * @Version:v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/SettingDemo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+/**
+ * @Author:wzr
+ * @Description:TODO
+ * @Create:2022/11/26 - 19:38
+ * @Version:v1.0
+ */
+public class SettingDemo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list1 =new ArrayList<>();
+        System.out.print("hhh....");
+        System.out.println("hello idea!");
+    }
+}
